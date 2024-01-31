4949d214ee69ac1dfd1c3791bfcd00a9fe6ebba5
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2024-01-31 23:08:05 +0530

    Allowed Acess

--- src/app.js
+++ src/app.js
@@ -11,7 +11,12 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://frontend-quizzie-m83lkudly-suman138-bbs.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
